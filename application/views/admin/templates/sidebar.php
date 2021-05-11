29167278aa3c6880c80b11288b100152a34e7ec4
mrizkyff <muhamadrizkyff@gmail.com>
2021-05-11 15:43:29 +0700

    sidebar

--- application/views/admin/templates/sidebar.php
+++ application/views/admin/templates/sidebar.php
@@ -174,10 +174,18 @@
                with font-awesome or any other icon font library -->
           <li class="nav-item">
             <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+                <span class="right badge badge-danger">Baru</span>
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-users"></i>
               <p>
                 Konsumen
-                <span class="right badge badge-danger">New</span>
               </p>
             </a>
           </li>
